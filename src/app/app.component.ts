ea0602f8f40cd040ab894a4bd31b995532bd8ea2
Pablo Ledesma <pabloledes83@gmail.com>
2017-02-20 23:47:25 -0500

    create notice class and mock notices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Notice {
+	id: number;
+	title: string;
+	description: string;
+	place: string;
+	date: number; // timestamps
+}
+
+const NOTICES: Notice[] = [
+	{ id: 1, title: 'first notice', description: 'description', place: 'Rome', date: Math.floor(Date.now()) },
+	{ id: 2, title: 'second notice', description: 'description', place: 'Brasilia', date: Math.floor(Date.now()) },
+	{ id: 3, title: 'thirth notice', description: 'description', place: 'Bogotá', date: Math.floor(Date.now()) },
+	{ id: 4, title: 'fourth notice', description: 'description', place: 'Las Vegas', date: Math.floor(Date.now()) },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  notices = NOTICES;
 }
